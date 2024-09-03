ef77e7205243f06a388e995402a636b24d1dbf94
Mike <45664417+thewhobox@users.noreply.github.com>
2024-09-03 19:49:37 +0200

    added search for esp32

--- Platforms/ESP32_Platform.cs
+++ Platforms/ESP32_Platform.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using OpenKNX.Toolbox.Lib.Data;
 
 namespace OpenKNX.Toolbox.Lib.Platforms;
@@ -10,7 +12,50 @@ public class ESP32_Platform : IPlatform
     {
         List<PlatformDevice> devices = new();
 
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            await FindEsp32Windows(devices);
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            await FindEsp32Linux(devices);
+
         return devices;
+    }
+
+    private async Task FindEsp32Windows(List<PlatformDevice> devices)
+    {
+        string command = "$portList = get-pnpdevice -class Ports\r\n" +
+                    "foreach ($usbDevice in $portList) {\r\n" + 
+                        "\tif ($usbDevice.Present) {\r\n" +
+                            "\t\t$isPico = $usbDevice.InstanceId.StartsWith('USB\\VID_1A86')\r\n" +
+                            "\t\t$isCom = $usbDevice.Name -match 'COM\\d{1,3}'\r\n" +
+                            "\t\tif ($isPico) {\r\n" +
+                                "\t\t\t$port = $Matches[0]\r\n" +
+                                "\t\t\tWrite-Host \"$port\"\r\n" +
+                            "\t\t}\r\n" +
+                        "\t}\r\n" +
+                    "}";
+        using var proc = new Process { StartInfo = { 
+            UseShellExecute = false, 
+            FileName = "powershell.exe",
+            RedirectStandardOutput = true,
+            CreateNoWindow = true,
+            Arguments = command
+        } };
+        proc.Start();
+        List<string> lines = new();
+        await proc.WaitForExitAsync();
+        while (!proc.StandardOutput.EndOfStream)
+        {
+            string line = await proc.StandardOutput.ReadLineAsync();
+            lines.Add(line);
+        }
+            
+        foreach(string port in lines)
+            devices.Add(new PlatformDevice(Architecture, "(Boot)", port, "boot"));
+    }
+
+    private async Task FindEsp32Linux(List<PlatformDevice> devices)
+    {
+        
     }
 
     public async Task DoUpload(PlatformDevice device, string firmwarePath, IProgress<KeyValuePair<long, long>>? progress = null)
